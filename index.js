abfa88bf3345e73a54dc6ebb160f12c19faa9e16
ashachakre0906 <chourpagar.asha@gmail.com>
2023-11-08 14:49:40 -0800

    added event listener to the all the button using for loop

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // var buttonClick = document.querySelector('.set');
 var drumEl = document.querySelectorAll('.drum');
 console.log(drumEl);
-// buttonClick.addEventListener('click', function () {
-//     alert('button clicked');
-// });
-drumEl.addEventListener('click', function () {
-    for (i = 0; i < drumEl.length; i++){
-        alert('button clicked');
-    }
-})
+
+for (i = 0; i < drumEl.length; i++){
+    console.log('button clicked', i);
+    drumEl[i].addEventListener('click', function () {
+     alert('I got clicked')
+ })
+}
