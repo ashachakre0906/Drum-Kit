943e016fd53a90803c51dc1a5fb72c5648abda9e
ashachakre0906 <chourpagar.asha@gmail.com>
2023-11-14 14:22:37 -0800

    added default statement in the switch statement

--- index.js
+++ index.js
@@ -15,34 +15,35 @@ for (i = 0; i < drumEl.length; i++) {
     //Using Switch Statement
     switch (buttonInnerHtml) {
       case "w":
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
+        var tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
         break;
       case "a":
-        var audio = new Audio("./sounds/tom-2.mp3");
-        audio.play();
+        var tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
         break;
       case "s":
-        var audio = new Audio("./sounds/tom-3.mp3");
-        audio.play();
+        var tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
         break;
       case "d":
-        var audio = new Audio("./sounds/tom-4.mp3");
-        audio.play();
+        var tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
         break;
       case "j":
-        var audio = new Audio("./sounds/snare.mp3");
-        audio.play();
+        var snare = new Audio("./sounds/snare.mp3");
+        snare.play();
         break;
       case "k":
-        var audio = new Audio("./sounds/kick-bass.mp3");
-        audio.play();
+        var kickbass = new Audio("./sounds/kick-bass.mp3");
+        kickbass.play();
         break;
       case "l":
-        var audio = new Audio("./sounds/crash.mp3");
-        audio.play();
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
         break;
-      default:
+        default:
+            console.log(buttonInnerHtml);
         break;
     }
     //Using ifelse statement
