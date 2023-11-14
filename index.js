76ec0fba185cbd4111582244409440aaec71e7ab
ashachakre0906 <chourpagar.asha@gmail.com>
2023-11-14 14:15:09 -0800

    alternatively adding the logic using switch statement

--- index.js
+++ index.js
@@ -12,7 +12,41 @@ for (i = 0; i < drumEl.length; i++) {
     console.log(this);
     var buttonInnerHtml = this.innerHTML;
     console.log(buttonInnerHtml);
-    if (buttonInnerHtml === "w") {
+    //Using Switch Statement
+    switch (buttonInnerHtml) {
+      case "w":
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+        break;
+      default:
+        break;
+    }
+    //Using ifelse statement
+    /* if (buttonInnerHtml === "w") {
       var audio = new Audio("./sounds/tom-1.mp3");
       audio.play();
     } else if (buttonInnerHtml === "a") {
@@ -33,7 +67,7 @@ for (i = 0; i < drumEl.length; i++) {
     } else if (buttonInnerHtml === "l") {
       var audio = new Audio("./sounds/kick-bass.mp3");
       audio.play();
-    }
+    } */
   });
 }
 //High order functions and functions as arguments
